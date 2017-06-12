141b72dd7113617b45be910f8ebcd0ca10c24e34
dags- <dags@dags.me>
2017-06-12 15:39:24 +0100

    version 2.2

--- src/main/java/me/dags/daflightmanager/sponge/DFMSponge.java
+++ src/main/java/me/dags/daflightmanager/sponge/DFMSponge.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 /**
  * @author dags <dags@dags.me>
  */
-@Plugin(name = "DaFlightManager", id = "daflightmanager", version = "2.1")
+@Plugin(name = "DaFlightManager", id = "daflightmanager", version = "2.2")
 public class DFMSponge implements RawDataListener {
 
     private static Config config = Config.defaultConfig();
